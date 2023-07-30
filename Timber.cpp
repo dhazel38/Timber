5847ccecb507748920413d7dc9033ec210db6b21
Dhaze laurent <dhazel@free.fr>
2023-07-30 17:35:54 +0200

    Chap5-low resolution

--- Timber.cpp
+++ Timber.cpp
@@ -32,12 +32,20 @@ side branchPositions[NUM_BRANCHES];
 int main()
 {
 	// Create a video mode object
-	VideoMode vm(1920, 1080);
+	// VideoMode vm(1920, 1080);
+
+	// Low res code
+	VideoMode vm(960, 540);
 
 	// Create and open a window for the game
 	//RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
 	//RenderWindow window(vm, "Timber!!!", Style::Resize);
+
+	// Low res code
 	RenderWindow window(vm, "Timber!!!");
+	View view(sf::FloatRect(0, 0, 1920, 1080));
+	window.setView(view);
+	//End of low res code!! Everything else is the same!!
 
 	// Create a texture to hold a graphic on the GPU
 	Texture textureBackground;
