d14aa2455749b1834762734a3bf568dd6e26cdfe
Dhaze laurent <dhazel@free.fr>
2023-08-03 16:49:45 +0200

    fin Chap11-part1

--- ZombieArena.cpp
+++ ZombieArena.cpp
@@ -6,6 +6,7 @@
 #include "Player.h"
 #include "ZombieArena.h"
 #include "TextureHolder.h"
+#include "Bullet.h"
 
 using namespace sf;
 
@@ -57,6 +58,25 @@ int main()
 	int numZombiesAlive;
 	Zombie* zombies = nullptr;
 
+	// 100 bullets should do
+	Bullet bullets[100];
+	int currentBullet = 0;
+	int bulletsSpare = 24;  // balles de rechange
+	int bulletsInClip = 6;  // balles dans clip
+	int clipSize = 6;  // taille des clips
+	float fireRate = 1;
+	// When was the fire button last pressed?
+	Time lastPressed;
+
+	// Hide the mouse pointer and replace it with crosshair
+	// Masquez le pointeur de la souris et remplacez-le par un réticule
+	window.setMouseCursorVisible(true);
+	Sprite spriteCrosshair;
+	Texture textureCrosshair = TextureHolder::GetTexture(
+		"graphics/crosshair.png");
+	spriteCrosshair.setTexture(textureCrosshair);
+	spriteCrosshair.setOrigin(25, 25);
+
 	// The main game loop
 	while (window.isOpen())
 	{
@@ -97,6 +117,26 @@ int main()
 
 				if (state == State::PLAYING)
 				{
+					// Reloading
+					if (event.key.code == Keyboard::R)
+					{
+						if (bulletsSpare >= clipSize)
+						{
+							// Plenty of bullets. Reload.
+							bulletsInClip = clipSize;
+							bulletsSpare -= clipSize;
+						}
+						else if (bulletsSpare > 0)
+						{
+							// Only few bullets left
+							bulletsInClip = bulletsSpare;
+							bulletsSpare = 0;
+						}
+						else
+						{
+							// More here soon?!
+						}
+					}
 				}
 
 			}
@@ -113,7 +153,7 @@ int main()
 		if (state == State::PLAYING)
 		{
 			// Handle the pressing and releasing of the WASD keys
-			if (Keyboard::isKeyPressed(Keyboard::W))
+			if (Keyboard::isKeyPressed(Keyboard::Z))
 			{
 				player.moveUp();
 			}
@@ -131,7 +171,7 @@ int main()
 				player.stopDown();
 			}
 
-			if (Keyboard::isKeyPressed(Keyboard::A))
+			if (Keyboard::isKeyPressed(Keyboard::Q))
 			{
 				player.moveLeft();
 			}
@@ -148,6 +188,28 @@ int main()
 			{
 				player.stopRight();
 			}
+			// Fire a bullet
+			if (Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				if (gameTimeTotal.asMilliseconds() 
+					- lastPressed.asMilliseconds() 
+					> 1000 / fireRate && bulletsInClip > 0)
+				{
+					// Pass the centre of the player
+					// and the centre of the cross-hair
+					// to the shoot function
+					bullets[currentBullet].shoot(
+						player.getCenter().x, player.getCenter().y,
+						mouseWorldPosition.x, mouseWorldPosition.y);
+					currentBullet++;
+					if (currentBullet > 99)
+					{
+						currentBullet = 0;
+					}
+					lastPressed = gameTimeTotal;
+					bulletsInClip--;
+				}
+			}// End fire a bullet
 
 		}// End WASD while playing
 
@@ -236,6 +298,9 @@ int main()
 			mouseWorldPosition = window.mapPixelToCoords(
 				Mouse::getPosition(), mainView);
 
+			// Set the crosshair to the mouse world location
+			spriteCrosshair.setPosition(mouseWorldPosition);
+
 			// Update the player
 			player.update(dtAsSeconds, Mouse::getPosition());
 
@@ -253,6 +318,15 @@ int main()
 					zombies[i].update(dt.asSeconds(), playerPosition);
 				}
 			}
+			// Update any bullets that are in-flight
+			for (int i = 0; i < 100; i++)
+			{
+				if (bullets[i].isInFlight())
+				{
+					bullets[i].update(dtAsSeconds);
+				}
+			}
+
 		}// End updating the scene
 
 		/*
@@ -278,8 +352,19 @@ int main()
 				window.draw(zombies[i].getSprite());
 			}
 
+			for (int i = 0; i < 100; i++)
+			{
+				if (bullets[i].isInFlight())
+				{
+					window.draw(bullets[i].getShape());
+				}
+			}
+
 			// Draw the player
 			window.draw(player.getSprite());
+
+			//Draw the crosshair
+			window.draw(	spriteCrosshair);
 		}
 
 		if (state == State::LEVELING_UP)
